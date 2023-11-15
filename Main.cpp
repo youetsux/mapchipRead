5d92344a4bd7a63ca62b523d062b7345f38070ba
y.sato <y.sato@jc-21.jp>
2023-11-15 16:36:02 +0900

    スクロールを経過時間ベースに変更

--- Main.cpp
+++ Main.cpp
@@ -6,35 +6,41 @@ const Size WORLD_SIZE{ 256 * 2, 192 * 2 };
 const SizeF CHR_RENDER_SIZE{ 32, 32 };
 constexpr Size WORLD_CHIP_SIZE{ 16,12 }; //ちっぷがわーるどじょうにたてよこなんこずつならんでいるか
 
-//カメラの中心位置（ワールド座標で指定）
-Vec2 cameraPos{ SCREEN_SIZE.x / 2, SCREEN_SIZE.y / 2 };
-//カメラの原点位置（ワールド座標で指定）
-Vec2 camaraOrigin{ cameraPos - SCREEN_SIZE / 2.0 };
+	namespace CAMERA2D {
+		//カメラの中心位置（ワールド座標で指定）
+		Vec2 cameraPos{ SCREEN_SIZE.x / 2, SCREEN_SIZE.y / 2 };
+		//カメラの原点位置（ワールド座標で指定）
+		Vec2 camaraOrigin{ cameraPos - SCREEN_SIZE / 2.0 };
 
-RectF cameraRect{ camaraOrigin,SCREEN_SIZE };//左上がカメラ原点、幅高さがスクリーンサイズになる矩形
+		RectF cameraRect{ camaraOrigin,SCREEN_SIZE };//左上がカメラ原点、幅高さがスクリーンサイズになる矩形
 
 
-void SetCameraPos(Vec2 _pos)
-{
-	cameraPos = _pos;
-	camaraOrigin = { cameraPos - SCREEN_SIZE / 2.0 };
-	cameraRect = { camaraOrigin,SCREEN_SIZE };
-}
-Vec2 GetScreenPosFromWorldPos(Vec2 _pos)
-{
-	return { _pos - camaraOrigin };
-}
+		void SetCameraPos(Vec2 _pos)
+		{
+			cameraPos = _pos;
+			camaraOrigin = { cameraPos - SCREEN_SIZE / 2.0 };
+			cameraRect = { camaraOrigin,SCREEN_SIZE };
+		}
+		Vec2 GetScreenPosFromWorldPos(Vec2 _pos)
+		{
+			return { _pos - camaraOrigin };
+		}
 
-void UpdateCamera() //アップデートと言いながら、マップからはみ出ないようにしているだけ
-{
-	if (cameraRect.x <= 0.0f)
-		SetCameraPos({ SCREEN_SIZE.x / 2.0f , cameraPos.y });
-		//cameraPos.x = SCREEN_SIZE.x / 2.0f;
-	if (cameraRect.y <= 0.0f)
-		SetCameraPos({ cameraPos.x , SCREEN_SIZE.y / 2.0f });
-		//cameraPos.y = SCREEN_SIZE.y / 2.0f;
+		void UpdateCamera() //アップデートと言いながら、マップからはみ出ないようにしているだけ
+		{
+			if (cameraRect.x <= 0.0f)
+				SetCameraPos({ SCREEN_SIZE.x / 2.0f , cameraPos.y });
 
-}
+			if (cameraRect.y <= 0.0f)
+				SetCameraPos({ cameraPos.x , SCREEN_SIZE.y / 2.0f });
+
+			if (cameraRect.x + cameraRect.w >= WORLD_SIZE.x)
+				SetCameraPos({ WORLD_SIZE.x - SCREEN_SIZE.x / 2 , cameraPos.y });
+
+			if (cameraRect.y + cameraRect.h >= WORLD_SIZE.y)
+				SetCameraPos({ cameraPos.x , WORLD_SIZE.y - SCREEN_SIZE.y / 2.0f });
+		}
+	}
 
 void Main()
 {
@@ -72,28 +78,29 @@ void Main()
 
 	while (System::Update())
 	{
-		const double MoveSpeed = 3.0;
+		const double MoveSpeed = 256/10.0;
+		//using namespace CAMERA2D;
 		if (KeyUp.pressed())
 		{
 			//SetCameraPos({ SCREEN_SIZE.x / 2, SCREEN_SIZE.y / 2 });
-			SetCameraPos(cameraPos + MoveSpeed * Vec2{ 0, -1 });
+			CAMERA2D::SetCameraPos(CAMERA2D::cameraPos + MoveSpeed * Vec2{ 0, -1 }*Scene::DeltaTime());
 		}
 		if (KeyRight.pressed())
 		{
 			//SetCameraPos({ SCREEN_SIZE.x + SCREEN_SIZE.x / 2, SCREEN_SIZE.y / 2 });
-			SetCameraPos(cameraPos + MoveSpeed * Vec2{ 1, 0 });
+			CAMERA2D::SetCameraPos(CAMERA2D::cameraPos + MoveSpeed * Vec2{ 1, 0 }*Scene::DeltaTime());
 		}
 		if (KeyLeft.pressed())
 		{
 			//SetCameraPos({ SCREEN_SIZE.x / 2, SCREEN_SIZE.y +SCREEN_SIZE.y / 2 });
-			SetCameraPos(cameraPos + MoveSpeed * Vec2{ -1, 0 });
+			CAMERA2D::SetCameraPos(CAMERA2D::cameraPos + MoveSpeed * Vec2{ -1, 0 }*Scene::DeltaTime());
 		}
 		if (KeyDown.pressed())
 		{
 			//SetCameraPos({ SCREEN_SIZE.x + SCREEN_SIZE.x / 2, SCREEN_SIZE.y + SCREEN_SIZE.y / 2 });
-			SetCameraPos(cameraPos + MoveSpeed * Vec2{ 0, 1 });
+			CAMERA2D::SetCameraPos(CAMERA2D::cameraPos + MoveSpeed * Vec2{ 0, 1 }*Scene::DeltaTime());
 		}
-		UpdateCamera();
+		CAMERA2D::UpdateCamera();
 
 		for (auto j = 0; j < WORLD_CHIP_SIZE.y; j++)
 		{
@@ -102,7 +109,7 @@ void Main()
 				//配列になってるオブジェクトのワールド座標
 				Vec2 wPos{ i * CHR_RENDER_SIZE.x, j * CHR_RENDER_SIZE.y };
 				//スクリーン座標に変換
-				Vec2 scrPos = GetScreenPosFromWorldPos(wPos);
+				Vec2 scrPos = CAMERA2D::GetScreenPosFromWorldPos(wPos);
 
 				mapchip(mapRects[CHR_MAP[j][i]]).draw(scrPos);
 			}
