6bdf8c757b3c9d29f83ec2a152103fc85908ac73
y.sato <y.sato@jc-21.jp>
2023-12-12 13:47:58 +0900

    もう一個修正

--- camera2D.h
+++ camera2D.h
@@ -1,10 +1,10 @@
 ﻿#pragma once
 
 
-Size WORLD_CHIP_SIZE{ 16,12 }; //ちっぷがわーるどじょうにたてよこなんこずつならんでいるか
-Size CHR_RENDER_SIZE{ 32, 32 };
-Size WORLD_SIZE{ CHR_RENDER_SIZE.x * WORLD_CHIP_SIZE.x, CHR_RENDER_SIZE.y * WORLD_CHIP_SIZE.y };
-Size SCREEN_SIZE{ WORLD_SIZE / 2 };
+constexpr Size WORLD_CHIP_SIZE{ 16,12 }; //ちっぷがわーるどじょうにたてよこなんこずつならんでいるか
+constexpr Size CHR_RENDER_SIZE{ 32, 32 };
+constexpr Size WORLD_SIZE{ CHR_RENDER_SIZE.x * WORLD_CHIP_SIZE.x, CHR_RENDER_SIZE.y * WORLD_CHIP_SIZE.y };
+constexpr Size SCREEN_SIZE{ WORLD_SIZE / 2 };
 
 
 namespace CAMERA2D {
